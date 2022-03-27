243fd6e1ce8efea7298a5ed4b86c403fdd0ec0b4
Wick.py <wickprof@gmail.com>
2022-03-27 14:21:44 +0500

    initial-commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x22419f94D0d4FABe138De67838082DcF7c3d671a";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
